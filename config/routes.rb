db238f8d6adc5613e03437f418626716297bcb49
Cesar Mayo <cesar.mayo18@gmail.com>
2019-09-02 16:24:47 -0400

    Create Welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :commerces
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  
+  get '/welcome', to: 'home#welcome'
 end
